08ce38df0fc219acfbb5ba50e31e59291f95fa94
estan <estan@infopro.com.my>
2025-04-14 14:50:04 +0800

    Update

--- listinglens.py
+++ listinglens.py
@@ -93,7 +93,7 @@ chrome_options.add_argument("--log-level=3")
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 chrome_options.add_argument('--disable-infobars')
 chrome_options.add_argument('--disable-extensions')
-chrome_options.binary_location = "/usr/bin/chromium-browser"
+chrome_options.binary_location = "/usr/bin/chromium"
 
 # --- Helper Functions ---
 def format_elapsed_time(start_time: float) -> str:
